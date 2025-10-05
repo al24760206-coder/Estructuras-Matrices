eb95caffa67ff036c88e684f30a84e5dc7345484
lian <al@24760206@ite.edu.mx>
2025-10-05 15:13:39 -0700

    Ejercicio 7 - suma de matrices listo

---
+++ Ejercicio 7 - suma de matrices .c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define FILAS 4
+#define COLUMNAS 4
+
+int main(){
+    
+    srand(time(NULL));
+
+    int matrizA[FILAS][COLUMNAS];
+    int matrizB[FILAS][COLUMNAS];
+    int matrizC[FILAS][COLUMNAS];
+    int i, j;
+
+    //matriz generada aleatoriamente con numeros de 1 a 50
+    printf("\nMatriz A aleatoriamente: \n");
+    for (i = 0; i <FILAS; i++){
+        for (j = 0; j < COLUMNAS; j++){
+
+            int num = rand() % 50;
+            matrizA [i][j] = num;
+
+            printf("%d\t",matrizA[i][j]);
+        }
+        printf("\n");
+    }
+    
+        printf("\nMatriz B aleatoriamente: \n");
+    for (i = 0; i <FILAS; i++){
+        for (j = 0; j < COLUMNAS; j++){
+
+            int num = rand() % 50;
+            matrizB [i][j] = num;
+
+            printf("%d\t",matrizB[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\nMatriz C (resultado): \n");
+    for (i = 0; i <FILAS; i++){
+        for (j = 0; j < COLUMNAS; j++){
+            
+            matrizC [i][j] = matrizA[i][j] + matrizB [i][j];
+            printf("%d\t",matrizC[i][j]);
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
